e7fa9b717e4788c83aba24bd2864f7322ff06df6
devbran <d-evbran@outlook.com>
2024-02-29 14:28:20 -0300

    folders solving

--- app/components/car/index.tsx
+++
@@ -1,9 +0,0 @@
-import CarRoot from "./CarRoot";
-import CarImage from "./CarImage";
-import CarAvailabilityIcon from "./CarAvailabilityIcon";
-
-export const Car = {
-  Root: CarRoot,
-  Image: CarImage,
-  AvailabilityIcon: CarAvailabilityIcon,
-};
