b2ee79ad4065d22bc69dd48f6a3d277cf14eb2c2
devbran <d-evbran@outlook.com>
2024-02-29 14:29:15 -0300

    folders solving rebase

---
+++ app/components/Car/index.tsx
@@ -0,0 +1,9 @@
+import CarRoot from "./CarRoot";
+import CarImage from "./CarImage";
+import CarAvailabilityIcon from "./CarAvailabilityIcon";
+
+export const Car = {
+  Root: CarRoot,
+  Image: CarImage,
+  AvailabilityIcon: CarAvailabilityIcon,
+};
